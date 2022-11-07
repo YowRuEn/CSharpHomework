0bf8982b03ca103e210544f72a26291c6618e281
YowRuEn <116792635+YowRuEn@users.noreply.github.com>
2022-11-07 13:00:49 +0800

    Add files via upload

---
+++ Birthday/Birthday/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Birthday
+{
+    internal class Program
+    {
+        ///<summary>
+        ///出生日期距离现在多少天
+        ///</summary>
+        static void Main(string[] args)
+        {
+            var birthday = new DateTime(2001, 06, 06);
+            var day = DateTime.Today - birthday;
+            Console.WriteLine("{0} day passed since my birthday");
+        }
+    }
+}
